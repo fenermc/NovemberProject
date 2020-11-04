084cc7957ca13f5734d38d78d1d8dc75701e27af
Muha-B20 <muhammeddcakmak@outlook.com>
2020-11-04 12:09:26 -0700

    Batman hsa Batmobile

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -4,5 +4,6 @@ public class Batman {
         System.out.println("I will catch you JOKER");
         //Yes he will catch him
         // Batman has cool Batmobile
+        //Batman has motocicyle
     }
 }
